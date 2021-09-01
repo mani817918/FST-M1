7dc077dbf60f2d877d87e27a562c888180d293e5
mani817918 <87297366+mani817918@users.noreply.github.com>
2021-09-01 11:26:16 +0530

    Add files via upload

---
+++ Cucumber/Activities/Cucumber/src/cucumberTest/GoogleSearchSteps.java
@@ -0,0 +1,43 @@
+package cucumberTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import cucumber.api.java.en.*;
+
+public class GoogleSearchSteps {
+    WebDriver driver;
+    WebDriverWait wait;
+    
+    @Given("^User is on Google Home Page$")
+    public void userIsOnGooglePage() throws Throwable {
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\ManikantaGaddavalasa\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe"); 
+        WebDriver driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 15);
+                
+        //Open the browser
+        driver.get("https://www.google.com");
+    }
+    
+    @When("^User types in Cheese and hits ENTER$")
+    public void userTypesInCheeseAndHitsENTER() throws Throwable {
+        driver.findElement(By.name("q")).sendKeys("Cheese", Keys.RETURN);
+    }
+
+    @Then("^Show how many search results were shown$")
+    public void showHowManySearchResultsWereShown() throws Throwable {
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("result-stats")));
+        String resultStats = driver.findElement(By.id("result-stats")).getText();
+        System.out.println("Number of results found: " + resultStats);
+    }
+
+    @And("^Close the browser$")
+    public void closeTheBrowser() throws Throwable {
+        driver.close();
+    }
+}
